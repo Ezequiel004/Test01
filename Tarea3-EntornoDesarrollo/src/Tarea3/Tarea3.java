96233bcc6d3615a74d0140440e81f000ddbb9ed1
Ezequiel Reina <ezequiel.s269491@cesurformacion.com>
2022-03-30 14:28:59 +0200

    Esta es la prueba de GitHub

---
+++ Tarea3-EntornoDesarrollo/src/Tarea3/Tarea3.java
@@ -0,0 +1,11 @@
+package Tarea3;
+import java.util.Scanner;
+public class Tarea3 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Esta es la prueba de GitHub");
+		sc.close();
+	}
+}
